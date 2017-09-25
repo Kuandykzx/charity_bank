0afd347b15a16b010edd419f41b47650128e98f8
Anuar <akhmetov.anuar09@gmail.com>
2017-09-25 08:40:59 +0600

    Initial commit

---
+++ charity_bank/users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.urlresolvers import reverse
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse('users:detail', kwargs={'username': self.username})
