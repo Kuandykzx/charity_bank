2ed0c7a1a6d6e88b149d03c7ddbfb8864d286804
Kuandykzx <kuandykzx@gmail.com>
2017-09-26 11:27:04 +0600

    front added

--- charity_bank/static/js/carousel.js
+++ charity_bank/static/js/carousel.js
@@ -1,18 +1,23 @@
-$('#recipeCarousel').carousel({
+('#recipeCarousel').carousel({
   interval: 10000
 })
 
-$('.carousel .carousel-item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
+(function(){
+    ('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
 
-    if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
+    for (var i=1;i<6;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
     }
-    else {
-      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-    }
-});
+  });
+}());
